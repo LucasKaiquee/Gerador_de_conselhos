ee71afa8eee0a82917e2482a033c3de3455de3d3
Lucas Kaique <lucaskaique743@gmail.com>
2022-10-01 18:06:46 -0300

    frist commit

---
+++ scripts/scripts.js
@@ -0,0 +1,44 @@
+//Consumindo a APi para gerar um conselho aleatório
+
+fetch('https://api.adviceslip.com/advice')
+  .then((response) => response.json())
+  .then((json) => {
+    createAdvice(json.slip.advice);
+  });
+
+const button = document.getElementById('gerar');
+const conselho = document.getElementById('conselho');
+const traduzir = document.getElementById('traduzir');
+
+function createAdvice(advice) {
+    conselho.innerHTML = advice;
+  }
+
+function newAdvice() {
+    window.location.reload();
+}
+
+button.addEventListener('click', newAdvice);
+
+//Fazendo o evento para mudar os tema de dark para White.
+
+const change = document.getElementById('change-theme');
+const img = document.getElementById('img');
+const background = document.getElementsByTagName('body')[0];
+const header = document.getElementsByTagName('header')[0];
+const main = document.getElementsByTagName('main')[0];
+
+function chengeTheme() {
+  background.classList.toggle('theme-withe');
+  header.classList.toggle('withe-header');
+  main.classList.toggle('withe');
+  button.classList.toggle('withe');
+  change.classList.toggle('change-color');
+
+  if(background.classList.contains('theme-withe')) {
+    img.src = './imgs/lua.png';
+  } else img.src = './imgs/sol.png';
+}
+
+change.addEventListener('click', chengeTheme);
+
